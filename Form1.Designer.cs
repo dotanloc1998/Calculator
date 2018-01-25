faa988785b440cf70cd87f872438e2bf57b2ebd3
Loc Do <thuyloc988@gmail.com>
2018-01-25 20:32:10 +0700

    Initial commit
    Written in C# Language
    Using Postfix algorithm

---
+++ Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Calculator
+{
+    partial class MiniCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.nut7 = new System.Windows.Forms.Button();
+            this.nut8 = new System.Windows.Forms.Button();
+            this.nut9 = new System.Windows.Forms.Button();
+            this.nut4 = new System.Windows.Forms.Button();
+            this.nut5 = new System.Windows.Forms.Button();
+            this.nut6 = new System.Windows.Forms.Button();
+            this.nut1 = new System.Windows.Forms.Button();
+            this.nut2 = new System.Windows.Forms.Button();
+            this.nut3 = new System.Windows.Forms.Button();
+            this.nut0 = new System.Windows.Forms.Button();
+            this.dauCham = new System.Windows.Forms.Button();
+            this.nutDelete = new System.Windows.Forms.Button();
+            this.nutXoaHet = new System.Windows.Forms.Button();
+            this.nutChia = new System.Windows.Forms.Button();
+            this.nutNhan = new System.Windows.Forms.Button();
+            this.nutTru = new System.Windows.Forms.Button();
+            this.nutCong = new System.Windows.Forms.Button();
+            this.dauBang = new System.Windows.Forms.Button();
+            this.moNgoac = new System.Windows.Forms.Button();
+            this.dongNgoac = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(359, 125);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // nut7
+            // 
+            this.nut7.Location = new System.Drawing.Point(17, 234);
+            this.nut7.Name = "nut7";
+            this.nut7.Size = new System.Drawing.Size(82, 50);
+            this.nut7.TabIndex = 1;
+            this.nut7.Text = "7";
+            this.nut7.UseVisualStyleBackColor = true;
+            this.nut7.Click += new System.EventHandler(this.nut7_Click);
+            // 
+            // nut8
+            // 
+            this.nut8.AccessibleName = "";
+            this.nut8.Location = new System.Drawing.Point(105, 234);
+            this.nut8.Name = "nut8";
+            this.nut8.Size = new System.Drawing.Size(82, 50);
+            this.nut8.TabIndex = 2;
+            this.nut8.Text = "8";
+            this.nut8.UseVisualStyleBackColor = true;
+            this.nut8.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // nut9
+            // 
+            this.nut9.Location = new System.Drawing.Point(193, 234);
+            this.nut9.Name = "nut9";
+            this.nut9.Size = new System.Drawing.Size(82, 50);
+            this.nut9.TabIndex = 3;
+            this.nut9.Text = "9";
+            this.nut9.UseVisualStyleBackColor = true;
+            this.nut9.Click += new System.EventHandler(this.nut9_Click);
+            // 
+            // nut4
+            // 
+            this.nut4.Location = new System.Drawing.Point(17, 290);
+            this.nut4.Name = "nut4";
+            this.nut4.Size = new System.Drawing.Size(82, 50);
+            this.nut4.TabIndex = 4;
+            this.nut4.Text = "4";
+            this.nut4.UseVisualStyleBackColor = true;
+            this.nut4.Click += new System.EventHandler(this.nut4_Click);
+            // 
+            // nut5
+            // 
+            this.nut5.Location = new System.Drawing.Point(105, 290);
+            this.nut5.Name = "nut5";
+            this.nut5.Size = new System.Drawing.Size(82, 50);
+            this.nut5.TabIndex = 5;
+            this.nut5.Text = "5";
+            this.nut5.UseVisualStyleBackColor = true;
+            this.nut5.Click += new System.EventHandler(this.nut5_Click);
+            // 
+            // nut6
+            // 
+            this.nut6.Location = new System.Drawing.Point(193, 290);
+            this.nut6.Name = "nut6";
+            this.nut6.Size = new System.Drawing.Size(82, 50);
+            this.nut6.TabIndex = 6;
+            this.nut6.Text = "6";
+            this.nut6.UseVisualStyleBackColor = true;
+            this.nut6.Click += new System.EventHandler(this.nut6_Click);
+            // 
+            // nut1
+            // 
+            this.nut1.Location = new System.Drawing.Point(17, 346);
+            this.nut1.Name = "nut1";
+            this.nut1.Size = new System.Drawing.Size(82, 50);
+            this.nut1.TabIndex = 7;
+            this.nut1.Text = "1";
+            this.nut1.UseVisualStyleBackColor = true;
+            this.nut1.Click += new System.EventHandler(this.nut1_Click);
+            // 
+            // nut2
+            // 
+            this.nut2.Location = new System.Drawing.Point(105, 346);
+            this.nut2.Name = "nut2";
+            this.nut2.Size = new System.Drawing.Size(82, 50);
+            this.nut2.TabIndex = 8;
+            this.nut2.Text = "2";
+            this.nut2.UseVisualStyleBackColor = true;
+            this.nut2.Click += new System.EventHandler(this.nut2_Click);
+            // 
+            // nut3
+            // 
+            this.nut3.Location = new System.Drawing.Point(193, 346);
+            this.nut3.Name = "nut3";
+            this.nut3.Size = new System.Drawing.Size(82, 50);
+            this.nut3.TabIndex = 9;
+            this.nut3.Text = "3";
+            this.nut3.UseVisualStyleBackColor = true;
+            this.nut3.Click += new System.EventHandler(this.nut3_Click);
+            // 
+            // nut0
+            // 
+            this.nut0.Location = new System.Drawing.Point(105, 402);
+            this.nut0.Name = "nut0";
+            this.nut0.Size = new System.Drawing.Size(82, 50);
+            this.nut0.TabIndex = 10;
+            this.nut0.Text = "0";
+            this.nut0.UseVisualStyleBackColor = true;
+            this.nut0.Click += new System.EventHandler(this.nut0_Click);
+            // 
+            // dauCham
+            // 
+            this.dauCham.Location = new System.Drawing.Point(193, 402);
+            this.dauCham.Name = "dauCham";
+            this.dauCham.Size = new System.Drawing.Size(82, 50);
+            this.dauCham.TabIndex = 11;
+            this.dauCham.Text = ".";
+            this.dauCham.UseVisualStyleBackColor = true;
+            this.dauCham.Click += new System.EventHandler(this.dauCham_Click);
+            // 
+            // nutDelete
+            // 
+            this.nutDelete.Location = new System.Drawing.Point(193, 178);
+            this.nutDelete.Name = "nutDelete";
+            this.nutDelete.Size = new System.Drawing.Size(82, 50);
+            this.nutDelete.TabIndex = 13;
+            this.nutDelete.Text = "Delete";
+            this.nutDelete.UseVisualStyleBackColor = true;
+            this.nutDelete.Click += new System.EventHandler(this.nutDelete_Click);
+            // 
+            // nutXoaHet
+            // 
+            this.nutXoaHet.Location = new System.Drawing.Point(17, 399);
+            this.nutXoaHet.Name = "nutXoaHet";
+            this.nutXoaHet.Size = new System.Drawing.Size(82, 50);
+            this.nutXoaHet.TabIndex = 14;
+            this.nutXoaHet.Text = "C";
+            this.nutXoaHet.UseVisualStyleBackColor = true;
+            this.nutXoaHet.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // nutChia
+            // 
+            this.nutChia.Location = new System.Drawing.Point(281, 178);
+            this.nutChia.Name = "nutChia";
+            this.nutChia.Size = new System.Drawing.Size(82, 50);
+            this.nutChia.TabIndex = 15;
+            this.nutChia.Text = "/";
+            this.nutChia.UseVisualStyleBackColor = true;
+            this.nutChia.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // nutNhan
+            // 
+            this.nutNhan.Location = new System.Drawing.Point(281, 234);
+            this.nutNhan.Name = "nutNhan";
+            this.nutNhan.Size = new System.Drawing.Size(82, 50);
+            this.nutNhan.TabIndex = 16;
+            this.nutNhan.Text = "X";
+            this.nutNhan.UseVisualStyleBackColor = true;
+            this.nutNhan.Click += new System.EventHandler(this.nutNhan_Click);
+            // 
+            // nutTru
+            // 
+            this.nutTru.Location = new System.Drawing.Point(281, 290);
+            this.nutTru.Name = "nutTru";
+            this.nutTru.Size = new System.Drawing.Size(82, 50);
+            this.nutTru.TabIndex = 17;
+            this.nutTru.Text = "-";
+            this.nutTru.UseVisualStyleBackColor = true;
+            this.nutTru.Click += new System.EventHandler(this.nutTru_Click);
+            // 
+            // nutCong
+            // 
+            this.nutCong.Location = new System.Drawing.Point(281, 343);
+            this.nutCong.Name = "nutCong";
+            this.nutCong.Size = new System.Drawing.Size(82, 50);
+            this.nutCong.TabIndex = 18;
+            this.nutCong.Text = "+";
+            this.nutCong.UseVisualStyleBackColor = true;
+            this.nutCong.Click += new System.EventHandler(this.nutCong_Click);
+            // 
+            // dauBang
+            // 
+            this.dauBang.Location = new System.Drawing.Point(281, 399);
+            this.dauBang.Name = "dauBang";
+            this.dauBang.Size = new System.Drawing.Size(82, 50);
+            this.dauBang.TabIndex = 19;
+            this.dauBang.Text = "=";
+            this.dauBang.UseVisualStyleBackColor = true;
+            this.dauBang.Click += new System.EventHandler(this.dauBang_Click);
+            // 
+            // moNgoac
+            // 
+            this.moNgoac.Location = new System.Drawing.Point(17, 178);
+            this.moNgoac.Name = "moNgoac";
+            this.moNgoac.Size = new System.Drawing.Size(82, 50);
+            this.moNgoac.TabIndex = 20;
+            this.moNgoac.Text = "(";
+            this.moNgoac.UseVisualStyleBackColor = true;
+            this.moNgoac.Click += new System.EventHandler(this.moNgoac_Click);
+            // 
+            // dongNgoac
+            // 
+            this.dongNgoac.Location = new System.Drawing.Point(105, 178);
+            this.dongNgoac.Name = "dongNgoac";
+            this.dongNgoac.Size = new System.Drawing.Size(82, 50);
+            this.dongNgoac.TabIndex = 21;
+            this.dongNgoac.Text = ")";
+            this.dongNgoac.UseVisualStyleBackColor = true;
+            this.dongNgoac.Click += new System.EventHandler(this.dongNgoac_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(336, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "label1";
+            // 
+            // MiniCalculator
+            // 
+            this.AcceptButton = this.dauBang;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 475);
+            this.Controls.Add(this.dongNgoac);
+            this.Controls.Add(this.moNgoac);
+            this.Controls.Add(this.dauBang);
+            this.Controls.Add(this.nutCong);
+            this.Controls.Add(this.nutTru);
+            this.Controls.Add(this.nutNhan);
+            this.Controls.Add(this.nutChia);
+            this.Controls.Add(this.nutXoaHet);
+            this.Controls.Add(this.nutDelete);
+            this.Controls.Add(this.dauCham);
+            this.Controls.Add(this.nut0);
+            this.Controls.Add(this.nut3);
+            this.Controls.Add(this.nut2);
+            this.Controls.Add(this.nut1);
+            this.Controls.Add(this.nut6);
+            this.Controls.Add(this.nut5);
+            this.Controls.Add(this.nut4);
+            this.Controls.Add(this.nut9);
+            this.Controls.Add(this.nut8);
+            this.Controls.Add(this.nut7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MiniCalculator";
+            this.Text = "MiniCalculator by LocDo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button nut7;
+        private System.Windows.Forms.Button nut8;
+        private System.Windows.Forms.Button nut9;
+        private System.Windows.Forms.Button nut4;
+        private System.Windows.Forms.Button nut5;
+        private System.Windows.Forms.Button nut6;
+        private System.Windows.Forms.Button nut1;
+        private System.Windows.Forms.Button nut2;
+        private System.Windows.Forms.Button nut3;
+        private System.Windows.Forms.Button nut0;
+        private System.Windows.Forms.Button dauCham;
+        private System.Windows.Forms.Button nutDelete;
+        private System.Windows.Forms.Button nutXoaHet;
+        private System.Windows.Forms.Button nutChia;
+        private System.Windows.Forms.Button nutNhan;
+        private System.Windows.Forms.Button nutTru;
+        private System.Windows.Forms.Button nutCong;
+        private System.Windows.Forms.Button dauBang;
+        private System.Windows.Forms.Button moNgoac;
+        private System.Windows.Forms.Button dongNgoac;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,405 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class MiniCalculator : Form
+    {
+        public MiniCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public class Stack<T>
+        {
+            class Node
+            {
+                public T data;
+                public Node next;
+                public Node(T a, Node b)
+                {
+                    data = a;
+                    next = b;
+                }
+            }
+            private Node head;
+            public int CountStack()
+            {
+                Node temp = head;
+                int i = 0;
+                for (; temp != null; temp = temp.next)
+                {
+                    i++;
+                }
+                return i;
+            }
+            public T Top()
+            {
+                return head.data;
+            }
+            public void Push(T a)
+            {
+                Node temp = new Node(a, head);
+                head = temp;
+            }
+            public void Pop()
+            {
+                head = head.next;
+            }
+            public bool IsEmpty()
+            {
+                if (head == null)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+        public string MidtoPosfix()
+        {
+            Stack<string> a = new Stack<string>();
+            string ketQuaHauTo = "";
+            string tokken;
+            string nhap = textBox1.Text;
+            nhap = nhap.Trim();
+            string s = "";
+            for (int i = 0; i < nhap.Length; i++)
+            {
+                if (nhap[i] != ' ')
+                {
+                    s += nhap[i];
+                }
+            }
+            int demMoNgoac = 0;
+            int demDongNgoac = 0;
+            int demToanTu = 0;
+            int demToanHang = 0;
+            for (int i = 0; i < s.Length;)
+            {
+                GetTokken(s, out tokken, ref i);
+                if (tokken[0] == '(')
+                {
+                    demMoNgoac++;
+                }
+                else if (tokken[0] == ')')
+                {
+                    demDongNgoac++;
+                }
+                else if (LaToanTu(tokken[0]))
+                {
+                    demToanTu++;
+                }
+                else
+                {
+                    demToanHang++;
+                }
+            }
+            if (demDongNgoac == demMoNgoac && demToanHang - 1 == demToanTu)
+            {
+                for (int i = 0; i < s.Length;)
+                {
+                    GetTokken(s, out tokken, ref i);
+                    if (!LaToanTu(tokken[0]))
+                    {
+                        ketQuaHauTo += tokken + " ";
+                    }
+                    else
+                    {
+                        if (tokken[0] == '(')
+                        {
+                            a.Push(tokken);
+                        }
+                        else if (tokken[0] == ')')
+                        {
+                            while (a.Top()[0] != '(')
+                            {
+                                ketQuaHauTo += a.Top() + " ";
+                                a.Pop();
+                            }
+                            if (a.Top()[0] == '(')
+                            {
+                                a.Pop();
+                            }
+                        }
+                        else
+                        {
+                            if (!a.IsEmpty() && DoUuTien(a.Top()[0]) < DoUuTien(tokken[0]))
+                            {
+                                a.Push(tokken);
+                                continue;
+                            }
+                            else if (a.IsEmpty())
+                            {
+                                a.Push(tokken);
+                                continue;
+                            }
+                            while (!a.IsEmpty() && DoUuTien(a.Top()[0]) >= DoUuTien(tokken[0]))
+                            {
+                                ketQuaHauTo += a.Top() + " ";
+                                a.Pop();
+                            }
+                            a.Push(tokken);
+                        }
+                    }
+                }
+                while (!a.IsEmpty())
+                {
+                    if (a.Top()[0] == '(')
+                    {
+                        a.Pop();
+                        continue;
+                    }
+                    ketQuaHauTo += a.Top() + " ";
+                    a.Pop();
+
+                }
+                return ketQuaHauTo;
+            }
+            else
+            {
+                return "\nBieu thuc sai";
+            }
+        }
+
+        public void SolvePosfix(string posfix)
+        {
+            string[] containValues = posfix.Split(' ');
+            Stack<string> containResult = new Stack<string>();
+            for (int i = 0; i < containValues.Length - 1; i++)
+            {
+                if (!LaToanTu(containValues[i][0]))
+                {
+                    containResult.Push(containValues[i]);
+                }
+                else
+                {
+                    string a = containResult.Top();
+                    containResult.Pop();
+                    string b = containResult.Top();
+                    containResult.Pop();
+                    containResult.Push(Calculate(containValues[i], b, a));
+                }
+            }
+            double kq = Convert.ToDouble(containResult.Top());
+            textBox1.Text = Convert.ToString(kq);
+        }
+
+        public string Calculate(string toanTu, string toanHang, string toanHang2)
+        {
+            double a = Convert.ToDouble(toanHang);
+            double b = Convert.ToDouble(toanHang2);
+            if (toanTu[0] == '+')
+            {
+                return Convert.ToString(a + b);
+            }
+            else if (toanTu[0] == '-')
+            {
+                return Convert.ToString(a - b);
+            }
+            else if (toanTu[0] == '*')
+            {
+                return Convert.ToString(a * b);
+            }
+            else if (toanTu[0] == '/' && b != 0)
+            {
+                return Convert.ToString(a / b);
+            }
+            return "?";
+        }
+        public void GetTokken(string chuoiNhap, out string tokken, ref int start)
+        {
+            tokken = "";
+            for (; start < chuoiNhap.Length; start++)
+            {
+                if (LaToanTu(chuoiNhap[start]))
+                {
+                    if (tokken == "")
+                    {
+                        tokken += chuoiNhap[start];
+                        start++;
+                        return;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    tokken += chuoiNhap[start];
+                }
+            }
+        }
+        public int DoUuTien(char a)
+        {
+            if (a == '+' || a == '-')
+            {
+                return 1;
+            }
+            else if (a == '*' || a == '/')
+            {
+                return 2;
+            }
+            return 0;
+        }
+        public bool LaToanTu(char a)
+        {
+            if (a == '+' || a == '-' || a == '*' || a == '/' || a == '(' || a == ')')
+            {
+                return true;
+            }
+            return false;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '/';
+            label1.Focus();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            label1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '8';
+            label1.Focus();
+        }
+
+        private void nut2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '2';
+            label1.Focus();
+        }
+
+        private void nut1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '1';
+            label1.Focus();
+        }
+
+        private void nut3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '3';
+            label1.Focus();
+        }
+
+        private void nut4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '4';
+            label1.Focus();
+        }
+
+        private void nut5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '5';
+            label1.Focus();
+        }
+
+        private void nut6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '6';
+            label1.Focus();
+        }
+
+        private void nut7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '7';
+            label1.Focus();
+        }
+
+        private void nut9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '9';
+            label1.Focus();
+        }
+
+        private void nutDelete_Click(object sender, EventArgs e)
+        {
+
+            string a = textBox1.Text;
+            textBox1.Text = "";
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                textBox1.Text += a[i];
+            }
+            label1.Focus();
+        }
+
+        private void nutNhan_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '*';
+            label1.Focus();
+        }
+
+        private void nutTru_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '-';
+            label1.Focus();
+        }
+
+        private void nutCong_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '+';
+            label1.Focus();
+        }
+
+        private void dauBang_Click(object sender, EventArgs e)
+        {
+            string posfix = MidtoPosfix();
+            try
+            {
+                SolvePosfix(posfix);
+            }
+            catch (Exception exception)
+            {
+                textBox1.Text = "Syntax Error";
+            }
+        }
+
+        private void dauCham_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '.';
+            label1.Focus();
+        }
+
+        private void nut0_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                textBox1.Text += "0";
+            }
+            label1.Focus();
+        }
+
+        private void moNgoac_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '(';
+            label1.Focus();
+        }
+
+        private void dongNgoac_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += ')';
+            label1.Focus();
+        }
+    }
+}
